406d179157a37c0e33bb90dabb8fc1739af8ac40
Vishnu Sundaresan <68603272+juliussneezer04@users.noreply.github.com>
2023-04-15 22:34:24 -0400

    remove auth0

--- pages/forums/index.tsx
+++ pages/forums/index.tsx
@@ -1,13 +1,7 @@
 import DefaultLayout from "@/components/defaultLayout";
-import Loading from "@/components/loading";
-import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
 export default function Forums() {
-  const { user, isAuthenticated, isLoading } = useAuth0();
-  if (isLoading) {
-    return <Loading />;
-  }
   return (
     <DefaultLayout>
       <div>
