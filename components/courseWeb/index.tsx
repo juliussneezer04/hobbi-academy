af4f9e24b48ad9ad74ba342c9b1cea2cdf6c5866
Vishnu Sundaresan <68603272+juliussneezer04@users.noreply.github.com>
2023-04-16 05:53:53 -0400

    alingment

--- components/courseWeb/index.tsx
+++ components/courseWeb/index.tsx
@@ -14,8 +14,8 @@ export default function CourseWeb(props: CourseWebProps) {
   return (
     <div className="opacity-100">
       <div className="flex flex-col items-center space-y-2">
-        <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl items-center">
-          <span>Your {" "} <img className="ml-8 h-[80px]" src="/hobbiLogo.png" /> Journey</span>
+        <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl flex flex-row items-center">
+          Your {" "} <img className="mx-2 h-[100px]" src="/hobbiLogo.png" /> Journey
         </h1>
         <p className="text-xl text-gray-500">
           Click on a course to learn more
